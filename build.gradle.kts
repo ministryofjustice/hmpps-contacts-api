5cc6411dc1718b509b0d7115a88c011c0df58ab3
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2024-07-31 15:32:11 +0100

    PRC-8 : Flyway migrations - basic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  id("org.openapi.generator") version "7.5.0"
   kotlin("plugin.spring") version "2.0.0"
 }
 
@@ -10,6 +13,24 @@ configurations {
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+// Database dependencies
+  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
+
+  // OpenAPI
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  // Test dependencies
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+  testImplementation("com.h2database:h2")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit:3.2.2")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+  testImplementation("net.javacrumbs.json-unit:json-unit-json-path:3.2.2")
+  testImplementation("org.springframework.security:spring-security-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.wiremock:wiremock-standalone:3.5.4")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.0.2")
   testImplementation("org.wiremock:wiremock-standalone:3.8.0")
@@ -20,7 +41,7 @@ kotlin {
 }
 
 tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  withType<KotlinCompile> {
     compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
   }
 }
