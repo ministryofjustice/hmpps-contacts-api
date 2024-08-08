0712cd23558e8d13d1b45b78b401c4e519bd26fd
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2024-08-08 17:42:29 +0100

    PRC-20 : add endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/resource/PrisonerController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/resource/PrisonerController.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppscontactsapi.client.prisonersearch.Prisoner
@@ -11,14 +12,11 @@ import uk.gov.justice.digital.hmpps.hmppscontactsapi.service.PrisonerService
 
 @Tag(name = "Get Prisoner")
 @RestController
-@RequestMapping(value = ["prisoner"], produces = [MediaType.APPLICATION_JSON_VALUE])
-class ContactTypesController(private val prisonerService: PrisonerService) {
-
-  @GetMapping(value = ["/prisoner"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping(value = ["contacts"], produces = [MediaType.APPLICATION_JSON_VALUE])
+class PrisonerController(private val prisonerService: PrisonerService) {
+  @GetMapping(value = ["/prisoner/{prisonerNumber}"], produces = [MediaType.APPLICATION_JSON_VALUE])
   @PreAuthorize("hasAnyRole('PRISONER_SEARCH')")
-  fun getScheduleForPrison(): Prisoner {
-    val prisonerNumber = "123"
-    val prisonCode = "joel"
-    return prisonerService.validatePrisonerAtPrison(prisonerNumber, prisonCode)
-  }
+  fun getPrisonerAtPrison(
+    @PathVariable("prisonerNumber") prisonerNumber: String,
+  ): Prisoner? = prisonerService.getPrisoner(prisonerNumber)
 }
