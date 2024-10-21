4ed7f1c01ecc36aca83c0970f4b375dc49a81439
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2024-10-21 10:42:20 +0100

    PRC-206 : patch contact - add integration tests , serializers and deserializers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/request/patch/util/PatchableSerializer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/request/patch/util/PatchableSerializer.kt
@@ -7,14 +7,11 @@ import com.fasterxml.jackson.databind.SerializerProvider
 class PatchableSerializer : JsonSerializer<Patchable<*>>() {
 
   override fun serialize(value: Patchable<*>?, gen: JsonGenerator, serializers: SerializerProvider) {
-    // Only serialize the value if it's not null
     if (value != null) {
       when (value) {
         is Patchable.Present<*> -> {
-          // Write the field and value for Patchable.Present
           val presentValue = value.get()
           if (presentValue != null) {
-            // Assuming `presentValue` is the value to be serialized directly
             gen.writeObject(presentValue)
           }
         }
